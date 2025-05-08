a2b3b91959355f671d55d0a0d188c44de118657e
JairAlmaguer <alan.almaguer@cceo.com.mx>
2025-05-08 10:29:49 -0600

    feat :sparkles: display pokemons list usin PokeAPI

--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,3 +1,27 @@
+<script setup>
+    import axios from 'axios';
+    import { ref } from 'vue';
+
+    const pokemons = ref([])
+
+    const getData = async () => {
+        try{
+            const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon');
+            console.log(data.results)
+            pokemons.value = data.results;
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    getData()
+
+</script>
+
 <template>
-    <h1>Pokemons</h1>
+    <ul>
+        <li v-for="poke in pokemons">
+            {{ poke.name }}
+        </li>
+    </ul>
 </template>
